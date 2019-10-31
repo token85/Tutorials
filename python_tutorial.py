865404438c70f1d3a29af4a4b193173685a79b7b
token <Tomaszkenig@Gmail.com>
2019-10-31 21:03:20 +0100

    Create python_tutorial.py

---
+++ python_tutorial.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 06:42:56 2019
+
+@author: token
+"""
+### Help
+help(str)
+
+
+### Numbers
+
+# Integer number example
+print(3)
+
+# value type
+type(33)
+
+# Float example
+print(3.14)
+
+# complex numbers
+# "j" sign and parenthesis
+x = (15 + 5j)
+
+# check datatype
+type(x)
+
+
+### Strings
+
+'Hello Wordld'
+
+# or
+
+"Hello World"
+
+# String concatenation and variables
+a = "Hello "
+b = "World"
+c = a+b
+
+print(c)
+
+# is is eqwual to:
+print("Hello "+'World')
+
+
+# string multiplication
+"Hello World! " * 3
+
+# Substring from left. Numbers from 0
+"Hello World! "[1]
+"Hello World! "[6:12]
+# or
+var = "Sample text"
+var[7]
+
+# Substring from right
+"Hello World! "[-5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
